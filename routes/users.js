d3150c2bcf66586d40c94ee16544866fa00f8eb4
fahemGithub <fahemeducaton@gmail.com>
2023-11-17 16:59:08 +0100

    Feat(users): fetch all users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,22 @@
 import express from "express"
 const router = express.Router()
 
+const users = [
+    {
+        "name": "fahem",
+        "last_name": "Chettout",
+        "age": 31
+    },
+    {
+        "name": "reda",
+        "last_name": "Bekka",
+        "age": 28
+    }
+]
+
+// Get all users
 router.get('/', (req, res) => {
-    res.send("hello")
+    res.send(users)
 })
 
 
