a1e757560c51d69046fe213ce9c7571afd51b84f
fahemGithub <fahemeducaton@gmail.com>
2023-11-17 17:28:21 +0100

    Feat(user): get one user

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,13 @@ const router = express.Router()
 
 const users = [
     {
+        "id": 1,
         "name": "fahem",
         "last_name": "Chettout",
         "age": 31
     },
     {
+        "id": 2,
         "name": "reda",
         "last_name": "Bekka",
         "age": 28
@@ -17,6 +19,15 @@ const users = [
 // Get all users
 router.get('/', (req, res) => {
     res.send(users)
+})
+
+// Get user by id
+router.get("/:id", (req, res) => {
+    const {id} = req.params
+    const user = users.find((item) => {
+        return item.id == id
+    })
+    res.send(user)
 })
 
 
