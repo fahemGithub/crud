6de74146b9aa280935bb315013361b3c6a864ae3
fahemGithub <fahemeducaton@gmail.com>
2023-11-17 17:37:40 +0100

    Feat(global): Add env file

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import bodyParser from "body-parser"
 import express from "express"
 import router  from "./routes/users.js"
 
+import dotenv from 'dotenv' 
+dotenv.config()
+console.log(process.env.PORT)
+
 const app = express()
-const port = 5000
 
 app.use(bodyParser.json())
 
 app.use('/users', router)
 
-app.listen(port, () => {
-    console.log(`Iam running on port ${port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Iam running on port ${process.env.PORT}`)
 })
